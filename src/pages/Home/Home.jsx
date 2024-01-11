5974bd29c54abc59c02803c11ee9a19cbf06fbe0
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2024-01-12 02:01:55 +0600

    added navbar

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import AnimatedCursor from 'react-animated-cursor';
+
+const Home = () => {
+    return (
+        <div>
+            <AnimatedCursor
+                innerSize={10}
+                outerSize={10}
+                color='201,243,29'
+                outerAlpha={0.2}
+                innerScale={0.7}
+                outerScale={5}
+                clickables={[
+                    'a',
+                    'input[type="text"]',
+                    'input[type="email"]',
+                    'input[type="number"]',
+                    'input[type="submit"]',
+                    'input[type="image"]',
+                    'label[for]',
+                    'select',
+                    'textarea',
+                    'button',
+                    '.link'
+                ]}
+            />
+            <Navbar></Navbar>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
